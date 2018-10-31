02b4d92bee6498fa6c42a098bf8a5b674c2a49b3
dnahotiori <dnahotiori@163.com>
2018-10-31 18:19:08 +0800

    2

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+from flask import Flask, request, abort
 app = Flask(__name__)
 
 
 @app.route("/")
 def homeIndex():
+    value = request.args.get("sss")
+    print(value)
+    # abort(401)
     return "index"
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.run(debug=True, host="192.168.5.5", port=80)
