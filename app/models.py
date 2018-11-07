0eb731b77f1db3be6c2ae654a9b37ab22ce71f28
dnahotiori <dnahotiori@163.com>
2018-11-07 16:07:28 +0800

    1

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import time
+import uuid
+# Create your models here.
+
+
+class SysConfig(models.Model):
+    ID = models.CharField(max_length=36, primary_key=True)
+    Content = models.CharField(max_length=5000)
+    ConfigType = models.IntegerField()
+    Updated = models.DateTimeField(default=time.time())
